66886007584354ea11d27cb40728b3a274de0fbf
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-06-24 16:05:34 +0200

    expanded invoice

---
+++ Sources/Commerce/billing/invoice-details.swift
@@ -0,0 +1,89 @@
+import Foundation
+import plate
+
+public enum InvoiceAddressType: Sendable {
+    case billing
+    case shipping
+}
+
+public struct InvoiceAddressDetail: Sendable {
+    public let type: InvoiceAddressType
+    public let street: String // Some Street
+    public let number: String // 51
+    public let area: String // 1058PJ
+    public let place: String // Amsterdam
+    
+    public init(
+        type: InvoiceAddressType,
+        street: String,
+        number: String,
+        area: String,
+        place: String
+    ) {
+        self.type = type
+        self.street = street
+        self.number = number
+        self.area = area
+        self.place = place
+    }
+}
+
+public struct InvoiceClientContactDetails: Sendable {
+    public let email: String
+    public let phone: String
+}
+
+public struct InvoiceClientDetail: Sendable {
+    public let name: String
+    public let infix: String?
+    public let middle: String?
+    public let surname: String?
+    public let contact: InvoiceClientContactDetails 
+    
+    public init(
+        name: String,
+        infix: String?,
+        middle: String?,
+        surname: String?,
+        contact: InvoiceClientContactDetails
+    ) {
+        self.name = name
+        self.infix = infix
+        self.middle = middle
+        self.surname = surname
+        self.contact = contact
+    }
+
+    public var fullName: String {
+        var parts: [String] = [name]
+        if let infix = infix { parts.append(infix) }
+        if let middle = middle { parts.append(middle) }
+        if let surname = surname { parts.append(surname) }
+        return parts.joined(separator: " ")
+    }
+}
+
+public struct InvoiceDogDetail: Sendable {
+    public let name: String
+    public let chip: String?
+    public let breed: String?
+    public let birthdate: String?
+    
+    public init(
+        name: String,
+        chip: String?,
+        breed: String?,
+        birthdate: String?
+    ) {
+        self.name = name
+        self.chip = chip
+        self.breed = breed
+        self.birthdate = birthdate
+    }
+}
+
+public struct InvoiceDetails: Sendable {
+    public let client: InvoiceClientDetail
+    public let dog: InvoiceDogDetail
+    public let address: [InvoiceAddressDetail]
+}
--- Sources/Commerce/billing/invoice.swift
+++ Sources/Commerce/billing/invoice.swift
@@ -1,8 +1,31 @@
 import Foundation
+import plate
 import Structures
+import Extensions
+
+public enum InvoiceLineItemError: Error, Sendable {
+    case negativeRateNotAllowed
+    case negativeCountNotAllowed
+    // case invalidVATPercentage
+}
+
+// public struct InvoiceLineItemVAT: Sendable {
+//     public let percentage: Double
+//     public let amount: Double
+
+//     public init(percentage: Double, on gross: Double) throws {
+//         guard percentage >= 0 && percentage <= 100 else {
+//             throw InvoiceLineItemError.invalidVATPercentage
+//         }
+
+//         self.percentage = percentage
+//         self.amount = gross - (gross / (1 + (percentage / 100)))
+//     }
+// }
 
 public struct InvoiceLineItem: Identifiable, Sendable {
     public let id: UUID
+    public let parentId: UUID?
     public let name: String
     public let direction: ValueDirection
     public let count: Double
@@ -10,23 +33,59 @@ public struct InvoiceLineItem: Identifiable, Sendable {
     public let subtotal: Double
     
     public init(
-        id: UUID,
+        id: UUID = UUID(),
+        parentId: UUID?,
         name: String,
         direction: ValueDirection,
         count: Double,
         rate: Double
-    ) {
-        self.id = UUID()
+    ) throws {
+        guard count >= 0 else {
+            throw InvoiceLineItemError.negativeCountNotAllowed
+        }
+
+        guard rate >= 0 else {
+            throw InvoiceLineItemError.negativeRateNotAllowed
+        }
+
+        self.id = id
+        self.parentId = parentId
         self.name = name
         self.direction = direction
         self.count = count
-        self.rate = rate
-        self.subtotal = (count * rate)
+        self.rate = ( rate.orient(to: direction) )
+        self.subtotal = (count * self.rate)
     }
 }
 
+public struct InvoicePayment: Identifiable, Sendable {
+    public let id: UUID
+    public let amount: Double
+    
+    public init(
+        id: UUID = UUID(),
+        amount: Double
+    ) {
+        self.id = id
+        self.amount = amount.orient(to: .credit)
+    }
+}
 
 public struct InvoiceData: Identifiable, Sendable {
     public let id: Int
+    public let details: InvoiceDetails
     public let content: [InvoiceLineItem]
+    public let payments: [InvoicePayment]
+
+    public var netTotal: Double {
+        content.map(\.subtotal).reduce(0, +)
+    }
+
+    public var paymentTotal: Double {
+        payments.map(\.amount).reduce(0, +)
+    }
+
+    public var finalBalance: Double {
+        netTotal - paymentTotal
+    }
 }
